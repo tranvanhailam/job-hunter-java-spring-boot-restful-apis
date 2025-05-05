a265ab6df7da08e9780bb390eeaf1bc015c51d13
Trần Văn Hải Lâm <hailamtranvan@gmail.com>
2025-05-05 23:25:14 +0700

    init

---
+++ src/main/java/vn/kyler/job_hunter/JobHunterApplication.java
@@ -0,0 +1,18 @@
+package vn.kyler.job_hunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+// @SpringBootApplication
+public class JobHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHunterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/vn/kyler/job_hunter/controller/HelloController.java
@@ -0,0 +1,13 @@
+package vn.kyler.job_hunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return "Hello World !!!";
+    }
+}
